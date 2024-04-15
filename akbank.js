473fd2a36ea6a92b4d120c1c86be17fc987632c3
Can Altıparmak <can6parmak@gmail.com>
2024-04-15 16:33:27 +0300

    fix: akbank postprocessing

--- akbank.js
+++ akbank.js
@@ -1,12 +1,13 @@
-import { readJSON, open } from 'https://deno.land/x/flat/mod.ts'
+import { readJSON } from 'https://deno.land/x/flat/mod.ts'
+import { parse } from "https://deno.land/std@0.222.1/datetime/parse.ts";
 
 const filename = Deno.args[0]
 const data = await readJSON(filename)
 
-const { cur, date } = data.GetCurrencyRatesResult
-const file = await open("akbank.csv", { append: true });
-for (kur of cur) {
-  const zaman = Math.floor(new Date(date).getTime() / 1000);
+const { cur, date } = JSON.parse(data.GetCurrencyRatesResult)
+const file = await Deno.open("akbank.csv", { append: true });
+for (const kur of cur) {
+  const zaman = Math.floor(parse(date, "dd.MM.yyyy HH:mm:ss").getTime() / 1000);
   const { Title, DovizAlis, DovizSatis, USDCaprazKur, KurTuru } = kur
   const str = `${zaman},${Title},${DovizAlis},${DovizSatis},${USDCaprazKur},${KurTuru}\n`
   await file.write(new TextEncoder().encode(str));
