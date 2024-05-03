bf2836706c5fe87c99b8598100fd332aa8911ac1
Can ALTIPARMAK <can6parmak@gmail.com>
2024-05-03 13:51:01 +0300

    fix: akbank local time offset

--- akbank.js
+++ akbank.js
@@ -1,6 +1,8 @@
 import { readJSON } from 'https://deno.land/x/flat/mod.ts'
 import { parse } from "https://deno.land/std@0.222.1/datetime/parse.ts";
 
+const TIME_OFFSET = 3
+
 const filename = Deno.args[0]
 const data = await readJSON(filename)
 
@@ -9,7 +11,9 @@ const file = await Deno.open("akbank.csv", { append: true });
 for (const kur of cur) {
   // gişe değil, banka kuru
   if (kur.KurTuru === "08") {
-    const zaman = Math.floor(parse(date, "dd.MM.yyyy HH:mm:ss").getTime() / 1000);
+    const tarih = parse(date, "dd.MM.yyyy HH:mm:ss")
+    tarih.setHours(tarih.getHours() - TIME_OFFSET)
+    const zaman = Math.floor(tarih.getTime() / 1000);
     const { Title, DovizAlis, DovizSatis } = kur
     const str = `${zaman},${Title},${DovizAlis},${DovizSatis}\n`
     await file.write(new TextEncoder().encode(str));
